58864a7e9c0b53777d18665331d797f449db18e1
Isaac <isaacwildsecond@gmail.com>
2022-04-15 13:30:57 +0100

    added libary and JS function to start adding cards

--- script.js
+++ script.js
@@ -1,26 +1,82 @@
-let myLibrary = [];
-const bookCard = document.querySelector(".bookCard");
-const btnRead = document.querySelector(".btnRead");
-const btnDelete = document.querySelector(".btnDelete");
-btnRead.addEventListener("click", () => {
-  readBook();
-});
-btnDelete.addEventListener("click", () => {
-  deleteBook();
-});
+let myLibrary = [
+  {
+    'title':'The Hobbit',
+    'author':'J.R.R Tolkien',
+    'pages':295,
+    'readbook': true
+  }
+]
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, readbook) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.readbook = readbook
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, readbook) {
   // do stuff here
+  
+  const newBook = new Book(title, author, pages, readbook);
+  myLibrary.push(newBook);
+
 }
 
-function readBook(){
-  alert("I read this book!");
-}
+const bookContainer = document.querySelector('.cardGridContainer');
+const bookCardDiv = document.createElement('div');
+bookCardDiv.classList.add('bookCard');
 
-function deleteBook(){
-  alert("Delete this book!");
-}
+const bookCardTitle = document.createElement('p');
+bookCardTitle.textContent = 'Title:'
+bookCardTitle.classList.add('cardTitle');
+
+const bookCardAuthor = document.createElement('p');
+bookCardAuthor.textContent = 'Author:'
+bookCardAuthor.classList.add('cardAuthor');
+
+const bookCardPages = document.createElement('p');
+bookCardPages.textContent = 'Pages:'
+bookCardPages.classList.add('cardPages');
+
+const bookCardRead = document.createElement('p');
+bookCardRead.textContent = 'Read:'
+bookCardRead.classList.add('cardRead');
+
+const bookCardPages = document.createElement('button');
+bookCardPages.textContent = 'Read'
+bookCardPages.classList.add('cardPages');
+
+const bookCardPages = document.createElement('button');
+bookCardPages.textContent = 'Pages:'
+bookCardPages.classList.add('cardPages');
+
+bookContainer.appendChild(bookCardDiv);
+bookCardDiv.appendChild(bookCardTitle);
+bookCardDiv.appendChild(bookCardAuthor);
+bookCardDiv.appendChild(bookCardPages);
+bookCardDiv.appendChild(bookCardRead);
+bookCardDiv.appendChild(btnRead);
+bookCardDiv.appendChild(btnDelete);
+
+// function readBook(){
+//   alert("I read this book!")
+// }
+
+// NOT working at all!?!
+// const btnDelete = document.querySelector(".btnDelete");
+// btnDelete.addEventListener("click", () => {
+//   console.log("Test");
+// });
+
+
+// const cardGridContainer = document.querySelector(".cardGridContainer")
+
+// function addBook(){
+//   const bookCard = document.createElement("div");
+//   cardGridContainer.appendChild(bookCard);
+//   bookCard.classList.add("bookCard");
+//   const cardTitle = document.createElement("div");
+//   bookCard.appendChild(bookTitle);
+//   bookCard.cardTitle.add("cardTitle");
+
+// }
